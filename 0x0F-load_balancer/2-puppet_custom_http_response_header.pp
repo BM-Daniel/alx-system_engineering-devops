8a970a14632d0656aba002a2ed5ac470e84285d8
BM-Daniel <danbabereyir@gmail.com>
2023-05-05 11:56:24 +0000

    Work on task 1 and 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+# Add a custom http header response using puppet
+
+exec { 'command':
+  command  => 'sudo apt-get -y update;
+  sudo apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo service nginx restart',
+  provider => shell,
+}
+
