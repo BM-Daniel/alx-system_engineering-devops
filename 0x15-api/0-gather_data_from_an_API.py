8299e2aab38ca807f24ad3d260d8fa696ac7dff6
BM-Daniel <danbabereyir@gmail.com>
2023-06-12 22:43:54 +0000

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,15 +18,13 @@ if __name__ == "__main__":
             id = int(sys.argv[1])
             user_response = requests.get('{}/users/{}'.format(URL, id)).json()
             todo_response = requests.get('{}/todos'.format(URL)).json()
-            username = user_response.get('username')
+            username = user_response.get('name')
 
             todo = list(filter(lambda x: x.get('userId') == id, todo_response))
-            todo_total = len(todo)
             completed = list(filter(lambda x: x.get('completed'), todo))
-            completed_total = len(completed)
 
             print('Employee {} is done with tasks ({}/{}):'
-                  .format(username, completed_total, todo_total))
+                  .format(username, len(completed), len(todo)))
 
             for complete_task in completed:
                 print('\t {}'.format(complete_task.get('title')))
